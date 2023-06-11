f20b6ffefdb90f4cec6a9e7059a341cf4c7429f5
Trudy M. Gill <71855491+trudygill@users.noreply.github.com>
2023-06-11 12:38:30 -0400

    Create booklistdb.sql

---
+++ Khan Academy/booklistdb.sql
@@ -0,0 +1,11 @@
+/*
+Created by: T.GILL
+Create date: 06-03-2023
+Description: Khan Academy SQL Challenge: Create a book list database. The aim is to practice creating a table and inserting values.
+*/
+
+
+CREATE TABLE books (id INTEGER primary key, name TEXT, rating INTEGER);
+INSERT INTO books VALUES(1000, 'Pearl in the Sand', 5);
+INSERT INTO books VALUES(1001, 'Never Seduce a Scot', 4.5);
+INSERT INTO books VALUES(1002, 'The Gladiator', 5); 
