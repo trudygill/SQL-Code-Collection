7a929cc1a4433971e7be6d6cb8add851828a5df6
Trudy M. Gill <71855491+trudygill@users.noreply.github.com>
2023-06-11 22:18:09 -0400

    Update friends.sql

--- Codecademy/friends.sql
+++ Codecademy/friends.sql
@@ -1,17 +1,17 @@
+-- created the initial table 
 CREATE TABLE friends (
   id INTEGER,
   name TEXT,
   birthday DATE
 ); 
 
--- SELECT *
--- FROM friends; 
+-- confirming that the table was created 
+SELECT *
+FROM friends; 
 
+-- inserting records into the table 
 INSERT INTO friends (id, name, birthday)
 VALUES (1, 'Ororo Munroe', '1940-05-30');
-
--- SELECT *
--- FROM friends; 
 
 INSERT INTO friends (id, name, birthday)
 VALUES (2, 'Trish Michelle', '1998-01-14'); 
@@ -19,23 +19,24 @@ VALUES (2, 'Trish Michelle', '1998-01-14');
 INSERT INTO friends (id, name, birthday)
 VALUES (3, 'Simon Plenty', '2001-12-15');
 
--- SELECT *
--- FROM friends; 
+-- confirming again that the records were addded 
+SELECT *
+FROM friends; 
 
+-- updating one record in the table 
 UPDATE friends
 SET name = 'Storm'
 WHERE id = 2;
 
--- SELECT *
--- FROM friends; 
-
-
+-- adding a column to the table
 ALTER TABLE friends
 ADD email TEXT;
 
--- SELECT *
--- FROM friends; 
+-- confirming that the column was adde 
+SELECT *
+FROM friends; 
 
+-- inserting data into the previously added column 
 UPDATE friends
 SET email = 'storm@codecademy.com'
 WHERE id = 1;
@@ -48,11 +49,10 @@ UPDATE friends
 SET email = 'simon@codecademy.com'
 WHERE id = 3; 
 
-SELECT *
-FROM friends; 
-
+-- deleting a record from the table 
 DELETE FROM friends
 WHERE name = 'Storm';
 
+-- confirming that the record was deleted 
 SELECT *
 FROM friends; 
